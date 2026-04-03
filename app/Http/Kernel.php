04baf360572fc30640a3add974808409f5a73cd2
Almasyriqi <ikramalmasyriqi@gmail.com>
2026-04-03 18:27:22 +0700

    turn off middleware restrict IP

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \RealRashid\SweetAlert\ToSweetAlert::class,
-            \App\Http\Middleware\RestrictIP::class,
+            // \App\Http\Middleware\RestrictIP::class,
         ],
 
         'api' => [
